972511137586e0634519f232ad573cf73d5a6895
vovkulakartm <dmyterko.jw@gmail.com>
2025-01-03 14:24:51 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Astronomic_Catalogs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
