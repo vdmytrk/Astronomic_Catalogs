b1e66ee9d4f6617bc2974d9bf9d61ef5ceae1a10
vovkulakartm <dmyterko.jw@gmail.com>
2025-01-23 14:59:25 +0200

    Added Infrastructure -> ConnectionStringProvider.

---
+++ Infrastructure/ConnectionStringProvider.cs
@@ -0,0 +1,32 @@
+﻿using Astronomic_Catalogs.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
+using System.Net.NetworkInformation;
+
+namespace Astronomic_Catalogs.Infrastructure;
+
+public class ConnectionStringProvider
+{
+    public string ConnectionString { get; }
+
+    public ConnectionStringProvider(IConfiguration configuration)
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+            .AddEnvironmentVariables();
+        var azureConnectionString = Environment.GetEnvironmentVariable("DefaultConnectionAzure");
+
+        if (!string.IsNullOrEmpty(azureConnectionString))
+        {
+            ConnectionString = azureConnectionString;
+        }
+        else
+        {
+            ConnectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,74 +1,69 @@
 using Astronomic_Catalogs.Data;
+using Astronomic_Catalogs.Infrastructure;
 using Azure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
 
-namespace Astronomic_Catalogs
+namespace Astronomic_Catalogs;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddSingleton<ConnectionStringProvider>();
+        string connectionString = "";
+
+        builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Configuration
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-            builder.Configuration.AddEnvironmentVariables();
-
-            string connectionString;
-            var azureConnectionString = Environment.GetEnvironmentVariable("DefaultConnectionAzure");
-            if (!string.IsNullOrEmpty(azureConnectionString))
-            {
-                connectionString = azureConnectionString;
-            }
-            else
-            {
-                connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            }
-
-            // Add services to the container.
-            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            var connectionStringProvider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
+            connectionString = connectionStringProvider.ConnectionString;
+            options.UseSqlServer(connectionStringProvider.ConnectionString);
 
 #if (DEBUG)
             Console.WriteLine($"\n\n  USING ENVIROMENT NAME: {builder.Environment.EnvironmentName}");
             Console.WriteLine($"\n\n  USING CONNECTION STRING: {connectionString} \n\n");
 #endif
+        });
 
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
         }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        app.Run();
     }
 }
