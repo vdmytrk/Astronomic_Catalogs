34ba910066001e127d1250fecaecb9209ed882af
vovkulakartm <dmyterko.jw@gmail.com>
2025-01-23 15:14:55 +0200

    Add Infrastructure -> ConfigureServices

---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,36 @@
+﻿using Astronomic_Catalogs.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Astronomic_Catalogs.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        // Реєстрація ConnectionStringProvider
+        services.AddSingleton<ConnectionStringProvider>();
+
+        // Реєстрація ApplicationDbContext
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            var connectionStringProvider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
+            var connectionString = connectionStringProvider.ConnectionString;
+            options.UseSqlServer(connectionString);
+
+#if DEBUG
+            Console.WriteLine($"\n\n  USING ENVIRONMENT NAME: {environment.EnvironmentName}");
+            Console.WriteLine($"\n\n  USING CONNECTION STRING: {connectionString} \n\n");
+#endif
+        });
+
+        // Інші сервіси
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddControllersWithViews();
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 using Astronomic_Catalogs.Data;
 using Astronomic_Catalogs.Infrastructure;
-using Azure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Localization;
-using System.Diagnostics;
 
 namespace Astronomic_Catalogs;
 
@@ -16,27 +11,7 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddSingleton<ConnectionStringProvider>();
-        string connectionString = "";
-
-        builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
-        {
-            var connectionStringProvider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
-            connectionString = connectionStringProvider.ConnectionString;
-            options.UseSqlServer(connectionStringProvider.ConnectionString);
-
-#if (DEBUG)
-            Console.WriteLine($"\n\n  USING ENVIROMENT NAME: {builder.Environment.EnvironmentName}");
-            Console.WriteLine($"\n\n  USING CONNECTION STRING: {connectionString} \n\n");
-#endif
-        });
-
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
+        builder.Services.AddApplicationServices(builder.Configuration, builder.Environment);
 
         var app = builder.Build();
 
