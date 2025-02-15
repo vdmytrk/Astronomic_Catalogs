b2a2f5bc4faf680f914251bdf00ea6a15dfd64da
vovkulakartm <dmyterko.jw@gmail.com>
2025-02-15 19:20:14 +0200

    The NLogService.cs wes restored.

---
+++ Infrastructure/NLogIfrastructure/NLogService.cs
@@ -0,0 +1,18 @@
+﻿using NLog.Targets;
+using NLog;
+
+namespace Astronomic_Catalogs.Infrastructure.NLogIfrastructure;
+
+public class NLogService
+{
+    public void UpdateNLogDatabaseConnectionString(string connectionString)
+    {
+        var config = LogManager.Configuration;
+        var databaseTarget = config.FindTargetByName<DatabaseTarget>("Database");
+        if (databaseTarget != null)
+        {
+            databaseTarget.ConnectionString = connectionString;
+            LogManager.ReconfigExistingLoggers();
+        }
+    }
+}
