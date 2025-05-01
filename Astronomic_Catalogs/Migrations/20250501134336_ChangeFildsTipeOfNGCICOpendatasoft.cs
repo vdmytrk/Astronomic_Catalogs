bce9372a9317ae17ece37d26916623664f2d5cdd
vovkulakartm <dmyterko.jw@gmail.com>
2025-05-01 16:44:07 +0300

    Add migration ChangeFildsTipeOfNGCICOpendatasoft.

---
+++ Astronomic_Catalogs/Migrations/20250501134336_ChangeFildsTipeOfNGCICOpendatasoft.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Astronomic_Catalogs.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFildsTipeOfNGCICOpendatasoft : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OpenngcNotes",
+                table: "NGCICOpendatasoft_Extension",
+                type: "varchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(330)",
+                oldMaxLength: 330,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NedNotes",
+                table: "NGCICOpendatasoft_Extension",
+                type: "varchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(110)",
+                oldMaxLength: 110,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OpenngcNotes",
+                table: "NGCICOpendatasoft",
+                type: "varchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(330)",
+                oldMaxLength: 330,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NedNotes",
+                table: "NGCICOpendatasoft",
+                type: "varchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(110)",
+                oldMaxLength: 110,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OpenngcNotes",
+                table: "NGCICOpendatasoft_Extension",
+                type: "nvarchar(330)",
+                maxLength: 330,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NedNotes",
+                table: "NGCICOpendatasoft_Extension",
+                type: "nvarchar(110)",
+                maxLength: 110,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OpenngcNotes",
+                table: "NGCICOpendatasoft",
+                type: "nvarchar(330)",
+                maxLength: 330,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NedNotes",
+                table: "NGCICOpendatasoft",
+                type: "nvarchar(110)",
+                maxLength: 110,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldNullable: true);
+        }
+    }
+}
